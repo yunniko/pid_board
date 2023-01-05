dd5e49b4453da810f7b6d9a24af8cec57bc0d4ac
yunniko <yunniko@ya.ru>
2023-01-05 08:39:24 +0100

    Navigation toggle

--- public/js/main.js
+++ public/js/main.js
@@ -3,13 +3,26 @@ Date.prototype.timeNow = function () {
     return ((this.getHours() < 10) ? "0" : "") + this.getHours() + ":" + ((this.getMinutes() < 10) ? "0" : "") + this.getMinutes() + ":" + ((this.getSeconds() < 10) ? "0" : "") + this.getSeconds();
 };
 
-const navList = document.getElementsByClassName("nav");
+(function() {
+    const navItemsList = document.getElementsByClassName("nav");
+    const toggleNavigation = document.getElementsByClassName("nav-toggle").item(0);
+    const navContainer = document.getElementsByClassName("nav-container").item(0);
 
-let activeNavItem = navList.item(0);
-for (const navItem of navList) {
-    if (navItem.getAttribute('href') === window.location.pathname + window.location.search) {
-        activeNavItem = navItem;
-        break;
+    if (sessionStorage.getItem('nav-toggle-hidden') == 'true') {
+        navContainer.classList.add('hidden');
     }
-}
-activeNavItem.classList.add('active');
+
+    let activeNavItem = navItemsList.item(0);
+    for (const navItem of navItemsList) {
+        if (navItem.getAttribute('href') === window.location.pathname + window.location.search) {
+            activeNavItem = navItem;
+            break;
+        }
+    }
+    activeNavItem.classList.add('active');
+
+    toggleNavigation.onclick = function(e){
+        navContainer.classList.toggle('hidden');
+        sessionStorage.setItem('nav-toggle-hidden', navContainer.classList.contains('hidden'));
+    };
+})();
